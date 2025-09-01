77b8232d14758be03b3a58c21539d1766dc4c456
ginzzpy <riotsx03@gmail.com>
2025-09-01 13:36:34 +0700

    feat: initial setup

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -3,27 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Role extends Model
 {
-    /** @use HasFactory<\Database\Factories\PegawaiFactory> */
-    use HasFactory;
-
-    protected $guarded = [
-        'id',
-    ];
-
+    protected $guarded = ['id'];
     public $timestamps = false;
 
-    public function admins(): HasMany
+    public function users(): HasMany
     {
-        return $this->hasMany(Admin::class, 'role_id');
-    }
-
-    public function viewers(): HasMany
-    {
-        return $this->hasMany(Viewer::class, 'role_id');
+        return $this->hasMany(User::class);
     }
 }
