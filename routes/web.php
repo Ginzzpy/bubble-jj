8ec0669a0be30d0ddf6d2c806a74c830376be776
ginzzpy <riotsx03@gmail.com>
2025-09-02 08:08:47 +0700

    feat: add welcome route to web.php for homepage view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
-use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
 require __DIR__ . '/auth.php';
