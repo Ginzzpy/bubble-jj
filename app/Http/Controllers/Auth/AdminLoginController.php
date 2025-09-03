4cd760d6ce99aaa6e82a230d94b3d259266ec803
elsecx <alwafielgin@gmail.com>
2025-09-04 04:07:16 +0700

    feat: create logout logic

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -4,11 +4,22 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminLoginController extends Controller
 {
     public function loginForm(Request $request)
     {
         return spaRender($request, 'pages.auth.admin');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect()->route('login.user.view');
     }
 }
