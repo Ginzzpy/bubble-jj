de4f76be4f96a04979eb3ad89534236a2ca5a447
elsecx <alwafielgin@gmail.com>
2025-09-04 08:54:22 +0700

    fix: resize the logo to make it responsive

--- resources/views/layouts/user/navbar.blade.php
+++ resources/views/layouts/user/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container">
         <a class="navbar-brand" href="#">
-            <img src="{{ asset(SettingsHelper::get('logo')) }}" alt="logo" class="img-fluid" style="max-width: 150px; height: auto;">
+            <img src="{{ asset(SettingsHelper::get('logo')) }}" alt="logo" class="img-fluid" style="max-width: 100px; height: auto;">
         </a>
         <ul class="navbar-nav">
             <li class="nav-item">
