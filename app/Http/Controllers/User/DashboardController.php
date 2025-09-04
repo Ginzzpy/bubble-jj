8eb17fb92d76e859d776d0455afd7e63ef96eca9
elsecx <alwafielgin@gmail.com>
2025-09-04 11:09:20 +0700

    feat: setup user dashboard with improved layout and navbar

--- app/Http/Controllers/User/DashboardController.php
+++ app/Http/Controllers/User/DashboardController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\UploadMenu;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index(Request $request)
     {
-        return spaRender($request, 'pages.user.dashboard');
+        $data['menus'] = UploadMenu::rememberCache('menus_all', 3600, function () {
+            return UploadMenu::all();
+        });
+
+        return spaRender($request, 'pages.user.dashboard', $data);
     }
 }
--- resources/views/layouts/user/navbar.blade.php
+++ resources/views/layouts/user/navbar.blade.php
@@ -1,15 +1,21 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container">
-        <a class="navbar-brand" href="#">
-            <img src="{{ asset(SettingsHelper::get('logo')) }}" alt="logo" class="img-fluid" style="max-width: 100px; height: auto;">
-        </a>
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link text-danger" href="#!" id="logout-btn">
+<header class="app-header">
+    <div class="main-header-container container-fluid">
+        <div class="header-content-left">
+            <div class="header-element">
+                <div class="horizontal-logo">
+                    <a class="header-logo" href="{{ route('user.dashboard') }}">
+                        <img src="{{ asset(SettingsHelper::get('logo')) }}" alt="logo">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="header-content-right">
+            <div class="header-element d-flex align-items-center">
+                <a class="btn btn-danger" href="#!" id="logout-btn">
                     <i class='mdi mdi-logout'></i>
                     Logout
                 </a>
-            </li>
-        </ul>
+            </div>
+        </div>
     </div>
-</nav>
+</header>
